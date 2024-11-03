97682f519c2a3ebfb6f864613aafc6907f4d9bde
Aaron Murgatroyd <amurgshere@gmail.com>
2024-11-03 18:50:01 +1000

    Use command-line parameter for main path Moved checking code to Ryujinx specific implementation

--- LanguageLocaliser/Program.cs
+++ LanguageLocaliser/Program.cs
@@ -6,39 +6,12 @@ namespace LanguageLocaliser
 {
     static class Program
     {
-        const string jsonPaths = @"H:\Programming\Ryujinx\Ryujinx-GreemDev\src\Ryujinx\Assets\Locales";
-
         public static void Main(string[] args)
-        {
+        {   
+            var jsonPaths = args.Count() == 1 ? args[0] : ".";
             Console.WriteLine("Starting");
             var ti = new RyujinxTranslationItems();
             ti.ReadFromFolderAsync(jsonPaths).Wait();
-/*
-            var testTranslations = ti.TestableTranslations(LocaleInfo.Create("en", "US"));
-            var testResults = new GoogleTranslator().Translate(testTranslations.Where(tt => tt.From.Valid));
-
-            foreach(var tr in testResults)
-            {
-                var tt = 
-                    testTranslations
-                    .Where(ttitem => ttitem.ToLocale.Equals(tr.Locale))
-                    .FirstOrDefault();
-
-                if (!tt.From.Valid)
-                {
-                    Console.WriteLine($"Could not find testable translation for language {tt.ToLocale}");
-                }
-                else 
-                {
-                    var existingValue = ti[tt.From.Name, tt.ToLocale];
-                    if (existingValue != tr.Text)
-                    {
-                        Console.WriteLine($"Translation from {tt.From} to {tt.ToLocale} failed conversion test with existing value being '{existingValue}' and new value being '{tr.Text}'.");
-                    }
-                }
-            }
-*/
-            
             var requiredTranslations = ti.RequiredTranslations([LocaleInfo.Create("en", "US"), LocaleInfo.Create("fr", "FR")]);
             var translations = new GoogleTranslator().Translate(requiredTranslations);
             ti.AddOrReplace(translations);
--- LanguageLocaliser/RyujinxTranslationItems.cs
+++ LanguageLocaliser/RyujinxTranslationItems.cs
@@ -98,5 +98,32 @@ namespace LanguageLocaliser
         {
             return base.TestableTranslations(sourceLocale, (item) => !item.Text.Contains('{') && item.Text.Where(ch => ch == ' ').Count() >= 2);
         }
+
+        public void CheckLanguages()
+        {
+            var testTranslations = TestableTranslations(LocaleInfo.Create("en", "US"));
+            var testResults = new GoogleTranslator().Translate(testTranslations.Where(tt => tt.From.Valid));
+
+            foreach(var tr in testResults)
+            {
+                var tt = 
+                    testTranslations
+                    .Where(ttitem => ttitem.ToLocale.Equals(tr.Locale))
+                    .FirstOrDefault();
+
+                if (!tt.From.Valid)
+                {
+                    Console.WriteLine($"Could not find testable translation for language {tt.ToLocale}");
+                }
+                else 
+                {
+                    var existingValue = this[tt.From.Name, tt.ToLocale];
+                    if (existingValue != tr.Text)
+                    {
+                        Console.WriteLine($"Translation from {tt.From} to {tt.ToLocale} failed conversion test with existing value being '{existingValue}' and new value being '{tr.Text}'.");
+                    }
+                }
+            }
+        }
     }
 }
