07a2a4ccca5c1d9aa5ff7e90802c00e4dc792b99
Aaron Murgatroyd <amurgshere@gmail.com>
2024-11-03 18:58:07 +1000

    Formatting

--- LanguageLocaliser/Program.cs
+++ LanguageLocaliser/Program.cs
@@ -12,6 +12,7 @@ namespace LanguageLocaliser
             Console.WriteLine("Starting");
             var ti = new RyujinxTranslationItems();
             ti.ReadFromFolderAsync(jsonPaths).Wait();
+            
             var requiredTranslations = ti.RequiredTranslations([LocaleInfo.Create("en", "US"), LocaleInfo.Create("fr", "FR")]);
             var translations = new GoogleTranslator().Translate(requiredTranslations);
             ti.AddOrReplace(translations);
